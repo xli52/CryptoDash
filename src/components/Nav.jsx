4fd3001c446e769105905fabf5ed5449c4bc42b6
xli52 <lxmichael52@gmail.com>
2022-08-10 23:44:47 -0700

    added different layout design for different screen sizes

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import './Nav.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {faSearch, faU, faUser} from '@fortawesome/free-solid-svg-icons';
+import { faSearch, faUser, faBitcoinSign, faBars } from '@fortawesome/free-solid-svg-icons';
 
 export default function Nav() {
 
@@ -17,20 +17,24 @@ export default function Nav() {
 
   return (
     <nav className="navbar">
-      <div className="logo">CryptoDash</div>
-        <form className="searchbar" onSubmit={handleSubmit}>
-          <FontAwesomeIcon icon={faSearch} className="searchbar-icon" />
-          <input
-            className="searchbar-input"
-            type="text"
-            value={value}
-            onChange={handleChange}
-            placeholder="Search crypto coins..."
-          />
-        </form>
+      <div className="logo">
+        <FontAwesomeIcon icon={faBitcoinSign} className="logo-icon" />
+        <div className="logo-name">CryptoDash</div>
+      </div>
+      <form className="searchbar" onSubmit={handleSubmit}>
+        <FontAwesomeIcon icon={faSearch} className="searchbar-icon" />
+        <input
+          className="searchbar-input"
+          type="text"
+          value={value}
+          onChange={handleChange}
+          placeholder="Search crypto coins..."
+        />
+      </form>
       <div className="menu">
         <div className="menu-username">Xiang</div>
         <FontAwesomeIcon icon={faUser} className="menu-profile" />
+        <FontAwesomeIcon icon={faBars} className="menu-bars" />
       </div>
     </nav>
   );
